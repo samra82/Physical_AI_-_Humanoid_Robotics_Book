de51d177f660850a258af11aef35ebfcd06fc887
Samra Shafiq <samrashafiq82@gmail.com>
2025-12-06 19:45:42 +0500

    Update Docusaurus config with  production URL

--- my-book/docusaurus.config.ts
+++ my-book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://physical-ai-humanoid-robotics-book-steel.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -71,7 +71,7 @@ const config: Config = {
     navbar: {
       title: 'Physical AI & Humanoid Robotics',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Logo',
         src: 'img/favicon.png',
       },
       items: [
